909369d1d7c43a8c6f5d0359f82cbde2c5c1a3ed
Kattis91 <katya.svedmark@gmail.com>
2023-10-26 12:19:20 +0000

    Work with Methods as Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,8 @@ import './App.css';
 // import EventBinding from './components/EventBinding';
 // import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 // import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import NestingComponents from './components/NestingComponents';
+// import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
   return (
@@ -23,7 +24,8 @@ function App() {
      {/* <EventBinding /> */}
      {/* <ConditionalRenderingClass /> */}
      {/* <ConditionalRenderingFunctional connected={true}/> */}
-     <NestingComponents />
+     {/* <NestingComponents /> */}
+     <MethodsAsPropsParent />
     </div>
   );
 }
